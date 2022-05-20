511b6f5b25bc0c3d29f025378d73947b2cbd81e9
Adam <youseemebloody@gmail.com>
2022-05-20 18:52:29 +0300

    initial commit

--- M4DTV/App.js
+++ M4DTV/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/pages/Home';
 import MovieScreen from './src/pages/Movie';
 import StreamScreen from './src/pages/Stream';
@@ -14,66 +14,64 @@ import LoginScreen from './src/pages/Login';
 
 const Stack = createNativeStackNavigator();
 
-
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ contentStyle: { backgroundColor: "#000000" } }} >
+      <Stack.Navigator
+        screenOptions={{contentStyle: {backgroundColor: '#000000'}}}>
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Movie"
           component={MovieScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="tv"
           component={ShowScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Stream"
           component={StreamScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Shows"
           component={ShowsScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Movies"
           component={MoviesScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="History"
           component={HistoryScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Watchlist"
           component={WatchlistScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Search"
           component={SearchScreen}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-
 
 export default App;
